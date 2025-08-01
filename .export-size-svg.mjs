8590e45e38fc8e25e8a1145d33164112f46ff4c4
CRIMX <straybugs@gmail.com>
2025-08-01 16:37:57 +0800

    docs: add example default size

--- .export-size-svg.mjs
+++ .export-size-svg.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "./lib/main.mjs";
 export default defineConfig({
   out: "./docs/images",
   exports: [
+    { title: "*", code: `export * from "@wopjs/disposable"` },
     { title: "disposableStore", code: `export { disposableStore } from "@wopjs/disposable"` },
     { title: "disposableMap", code: `export { disposableMap } from "@wopjs/disposable"` },
     { title: "disposableOne", code: `export { disposableOne } from "@wopjs/disposable"` },
